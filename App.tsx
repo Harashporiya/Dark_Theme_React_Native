55fbf2d22d8431e3c7cd333e8c6ecd9d3d666314
Your Name <your_email@example.com>
2024-07-03 18:24:26 +0530

    Theme

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+
+export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <View className="flex-1 justify-center items-center dark:bg-neutral-900 space-y-6">
+      <StatusBar style={colorScheme == 'dark' ? 'light' : 'dark'} />
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl dark:text-white">Toggle Theme</Text>
+        <Switch
+          value={colorScheme == 'dark'}
+          onChange={toggleColorScheme}
+          style={{ transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }] }}
+        />
+      </View>
+      <Text className="mx-4 text-justify dark:text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Text>
+      <View className="h-48 w-full bg-sky-900 dark:bg-orange-300 ">
+      
+      </View>
+    </View>
+  );
+}
